d5fb21c9d53532d91c07f8394bdd7312fc9852a1
caique-caetano <caiquecaetano07@gmail.com>
2023-01-02 23:24:39 -0300

    Alura React

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner () {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner do site organo"/>
+        </header>
+    );
+
+}
+
+export default Banner
