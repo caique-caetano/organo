d990cd63ccb81edc7449b440189751be66f1a391
caique-caetano <caiquecaetano07@gmail.com>
2023-01-03 21:31:44 -0300

    Criando componente Time, e ajustando style. Anteriormente feito a captura de valores do formulario, com useState.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,16 +1,17 @@
+// import { useState } from 'react'
 import './CampoTexto.css'
 const CampoTexto = (props) => {
-    let valor = 'Caique Caetano'
+
+    const placeholderModificada = `${props.placeholder}...`
 
     const aoDigitado = (evento) => {
-            valor = evento.target.value
-            console.log(valor)
+            props.aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
         
     )
